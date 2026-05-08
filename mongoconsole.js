7cfb74c28aca4dc422496510633ae79e85dde81e
MarcelKainz <146081005+MarcelKainz@users.noreply.github.com>
2026-05-08 09:01:12 +0200

    29

--- mongoconsole.js
+++ mongoconsole.js
@@ -362,3 +362,27 @@ db.getSiblingDB("htlzwettl").getCollection("emps").aggregate([
                         }
                     }
                 ])
+
+        //29
+            db.emps.aggregate([
+                {
+                    $group: {
+                        _id: { dept_id: "$dept_id", JOB: "$JOB" },
+                        NUMBER_OF_EMPLOYEES: { $sum: 1 }
+                        }
+                    },
+                {
+                    $sort: {
+                        "_id.dept_id": 1,
+                        "_id.JOB": 1
+                        }
+                    },
+                {
+                    $project: {
+                        _id: 0,
+                        dept_id: "$_id.dept_id",
+                        JOB: "$_id.JOB",
+                        NUMBER_OF_EMPLOYEES: 1
+                        }
+                    }
+                ]);
